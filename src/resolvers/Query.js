c0a142e8a16a3323cb479d317ae8d425951d5c65
Stephen Ward <ward@Stephens-MacBook-Pro.local>
2018-07-22 20:40:22 -0700

    Reduce Complexity of User Query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,13 +1,11 @@
 const { getUserId } = require('../utils')
 
 const plants = (root, args, context, info) => context.db.query.plants({}, info)
-const users = (root, args, context, info) => {
-  if (getUserId(context)) {
-    return args.email
-      ? context.db.query.users({ where: { email: args.email } }, info)
-      : context.db.query.users({}, info)
-  }
-}
+const users = (root, args, context, info) =>
+  args.email
+    ? context.db.query.users({ where: { email: args.email } }, info)
+    : context.db.query.users({}, info)
+
 const images = (root, args, context, info) => context.db.query.images({}, info)
 const categories = (root, args, context, info) =>
   context.db.query.categories({}, info)
